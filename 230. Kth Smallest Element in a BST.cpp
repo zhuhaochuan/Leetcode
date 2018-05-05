b74cbe3c72cd53ce8cda9a9ef9ce6c08cb3a8902
朱昊川 <1525722736@qq.com>
2018-05-05 12:03:36 +0800

    中序遍历

---
+++ 230. Kth Smallest Element in a BST.cpp
@@ -0,0 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };分析：找到第k个最小的元素在BST
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        
+    }
+};
