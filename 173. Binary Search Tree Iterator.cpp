3a6d5b144fb100ad8017b56c8f73cef07dccd278
朱昊川 <1525722736@qq.com>
2018-05-09 20:27:15 +0800

    BST中序遍历 迭代实现

---
+++ 173. Binary Search Tree Iterator.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode *root) {
+        
+    }
+
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        
+    }
+
+    /** @return the next smallest number */
+    int next() {
+        
+    }
+};
+
+/**
+ * Your BSTIterator will be called like this:
+ * BSTIterator i = BSTIterator(root);
+ * while (i.hasNext()) cout << i.next();
+ */
